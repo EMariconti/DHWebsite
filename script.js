8f4b20a236ea9ef8a2049dbf0dfb48d0aa8005ea
EMariconti <enrico.mariconti.14@ucl.ac.uk>
2026-04-20 20:50:21 +0100

    rework colours and style, division in pages, filled home and started people page

---
+++ script.js
@@ -0,0 +1,14 @@
+// Mobile navigation toggle
+const toggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (toggle && navLinks) {
+    toggle.addEventListener('click', () => {
+        navLinks.classList.toggle('open');
+    });
+
+    // Close nav when a link is clicked (mobile)
+    navLinks.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', () => navLinks.classList.remove('open'));
+    });
+}
